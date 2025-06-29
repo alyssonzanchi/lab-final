4635665f723163c7bf6652bb59baf43d8a8f1a93
Alysson Zanchi <alyssonzanchi@gmail.com>
2025-06-29 18:37:06 -0300

    first commit

---
+++ src/main/java/org/example/HelloServlet.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = resp.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Meu Projeto Web</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Olá, Mundo! Deploy via Jenkins CI/CD funcionando!</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+}
